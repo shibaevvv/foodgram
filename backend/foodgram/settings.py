24c5235d0e795b6f0a46265f41c6122e6478c198
Vladimir Shibaev <vladimir.shibaev@gmail.com>
2025-12-03 16:42:01 +0300

    add secrets on git action

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -13,6 +13,10 @@ DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
 
+CSRF_TRUSTED_ORIGINS = [
+    origin for origin in os.environ.get('CSRF_TRUSTED_ORIGINS', '').split(',')
+    if origin
+]
 
 # Application definition
 
