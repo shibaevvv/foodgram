3b2152a4332b586b371b1cd4df795dace5a618c1
Vladimir Shibaev <vladimir.shibaev@gmail.com>
2025-12-05 09:04:26 +0300

    Fix views in recipes

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -1,13 +1,6 @@
-from django.http import Http404
 from django.shortcuts import redirect
-
-from recipes.models import Recipe
-
-RECIPE_NOT_FOUND = 'Рецепта с id = {} не существует!'
 
 
 def recipe_redirect(request, pk):
     """Метод для редиректа короткой ссылки на полный адрес рецепта."""
-    if Recipe.objects.filter(pk=pk).exists():
-        return redirect(f'/recipes/{pk}')
-    return Http404(RECIPE_NOT_FOUND.format(pk))
+    return redirect(f'/recipes/{pk}')
