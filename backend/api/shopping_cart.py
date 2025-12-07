b748f994c29dc41a2280c504755db4886348cfb9
Vladimir Shibaev <vladimir.shibaev@gmail.com>
2025-12-07 13:10:51 +0300

    Fix 3 review

--- backend/api/shopping_cart.py
+++ backend/api/shopping_cart.py
@@ -1,19 +1,21 @@
-from datetime import datetime
+from datetime import date
+
+from babel.dates import format_date
 
 SHOPPING_CART_TITLE = (
-    f'Список покупок от {datetime.now().strftime("%d.%m.%Y")}.'
+    f'Список покупок от {format_date(date.today(), "dd MMMM yyyy", locale="ru")}.'
 )
 SHOPPING_CART_RECIPES = '   \u2022 {} (Автор - {})'
 SHOPPING_CART_PRODUCTS = '  {}. {} - {} ({})'
 
 
-def get_shopping_cart_text(recipes, domain):
+def get_shopping_cart_text(ingredients, recipes, domain):
     """Метод для формирования текса скачиваемой корзины покупок."""
     return '\n'.join([
         SHOPPING_CART_TITLE,
         '-' * len(SHOPPING_CART_TITLE),
         '',
-        'Для приготовления следующих рецептов:',
+        'Для приготовления рецептов:',
         '',
         *{SHOPPING_CART_RECIPES.format(
             recipe['recipe__name'],
@@ -28,7 +30,7 @@ def get_shopping_cart_text(recipes, domain):
             ingredient['ingredient__name'].capitalize(),
             ingredient['sum'],
             ingredient['ingredient__measurement_unit']
-        ) for count, ingredient in enumerate(recipes, 1)],
+        ) for count, ingredient in enumerate(ingredients, 1)],
         '',
         '',
         f'Загружено с сайта: {domain}'
--- backend/recipes/urls.py
+++ backend/recipes/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from api.views import recipe_redirect
+from recipes.views import recipe_redirect
 
 urlpatterns = [
     path('s/<int:pk>/', recipe_redirect, name='short-link')
